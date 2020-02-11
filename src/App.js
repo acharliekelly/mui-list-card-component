bd39bb22d9eb138e79d26712c05b1da3513cf4cb
Charlie Kelly <charliekelly31415@gmail.com>
2020-02-11 18:32:28 -0500

    ready for MVP

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
-import { Button } from './components';
+import { ListCard } from './components';
 import './App.scss';
 
 class App extends Component {
   render() {
+    const itemList = [
+      'Apple',
+      'Orange',
+      'Banana',
+      'Strawberry',
+      'Pear',
+      'Plum',
+      'Kumquat',
+      'Mango',
+      'Watermelon'
+    ]
     return (
       <div className="App">
         <header>
           <h1>Components</h1>
         </header>
         <div className="component-list">
-          <Button href="#" theme='go'> Button </Button>
+          {/* <Button href="#" theme='go'> Button </Button> */}
+          <ListCard 
+            header="Fruit"
+            items={itemList}
+            theme="dark"
+          />
         </div>
       </div>
     );
