a518c5296c4e14980b946620250888cee2bf16f3
Charlie Kelly <charliekelly31415@gmail.com>
2020-02-11 18:35:24 -0500

    remove dynamic items for mvp

--- src/components/list-card/ListCard.stories.js
+++ src/components/list-card/ListCard.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { withKnobs, array, radios } from '@storybook/addon-knobs';
+import { withKnobs, radios } from '@storybook/addon-knobs';
 
 import { ListCard } from '../../components';
 
